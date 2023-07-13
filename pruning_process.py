98f0b3f231f508cb252b9ee6e0e9732609cb4ff3
Barış Yumak <byumak01@gmail.com>
2023-07-13 19:09:34 +0300

    - pruning_process.py added but not tested !!!

---
+++ pruning_process.py
@@ -0,0 +1,55 @@
+
+import eqs_and_variables as ev
+import draw
+
+
+def index_array():
+    arr = []
+    for i in range(ev.neuron_count):
+        arr.append(ev.pool_capacity + 1)
+    return arr
+
+
+# find_synapse_with_the_lowest_weight function finds synapse with the lowest weight outgoing from a neuron, for every
+# neuron in a layer.
+def find_synapse_with_the_lowest_weight(syn_obj):
+    indices_of_synapses_with_min_weight = index_array()
+    for idx in range(len(syn_obj.w)):
+        pre_idx = syn_obj.i[idx]
+
+        if syn_obj.w[idx] < indices_of_synapses_with_min_weight[pre_idx]:
+            indices_of_synapses_with_min_weight[pre_idx] = syn_obj.w[idx]
+
+    return indices_of_synapses_with_min_weight
+
+
+def give_weights_back_to_the_pool(layer_obj, syn_obj, indices_of_synapses_with_min_weight):
+    for syn_idx in range(len(syn_obj.w)):
+        if syn_idx not in indices_of_synapses_with_min_weight:
+            post_idx = syn_obj.j[syn_idx]
+
+            layer_obj.w_pool[post_idx] += syn_obj.w[syn_idx]
+            syn_obj.w[syn_idx] = 0
+
+
+def draw_after_pruning(layers, folder_path, synapse_objects):
+    draw.reset_board()
+    draw.draw_enabled_neurons(layers)
+    draw.draw_active_neurons_in_stimulus_layer(ev.inputs[ev.input_shape])
+
+    for layer_obj in layers:
+        layer_idx = layers.index(layer_obj)
+        for neuron_idx in range(ev.neuron_count):
+            draw.draw_current_state(layer_obj.v[neuron_idx], neuron_idx, layer_idx, 'white', False, synapse_objects[layer_idx])
+
+    draw.draw_outlines_layer_names_and_time(ev.run_time, folder_path, False)
+
+
+def pruning(layers, synapse_objects, folder_path):
+    len_syn_objects = len(synapse_objects)
+
+    for idx in range(0, len_syn_objects - 1):
+        indices_of_synapses_with_min_weight = find_synapse_with_the_lowest_weight(synapse_objects[idx])
+        give_weights_back_to_the_pool(layers[idx], synapse_objects[idx], indices_of_synapses_with_min_weight)
+
+    draw_after_pruning(layers, folder_path, synapse_objects)
